b3dcd5e7ed0c3bc0a71ad00ed3a00369e0eca315
Zayne.Liu <llz981011@hotmail.com>
2020-07-21 23:00:13 +0100

    search api

---
+++ js/index.js
@@ -0,0 +1,41 @@
+var searchBar = new Vue({
+	el: "#search-bar",
+	data: {
+		message: 'Hello Vue!',
+		keyword: ""
+	},
+	methods: {
+		searchBtnOnClick() {
+			googleSearch();
+		}
+	}
+});
+
+var searchResult = new Vue({
+	el: "#search-result",
+	data() {
+		return {
+			result: []
+		}
+	}
+});
+
+
+// Key 1: dbd26bf4d9f7408387421d019e8c7516
+function googleSearch() {
+	// const baseUrl = "http://localhost:5000/search?keyword=";
+	const baseUrl = "https://api.cognitive.microsoft.com/bing/v7.0/search?q=";
+	const sub_key = "dbd26bf4d9f7408387421d019e8c7516";
+	let keyword = searchBox.value;
+
+	axios({
+		method: "GET",
+		url: baseUrl + encodeURIComponent(keyword),
+		headers: {
+			"Ocp-Apim-Subscription-Key": sub_key
+		}
+	}).then((result) => {
+		searchResult.$data.result = result.data.webPages.value;
+		console.log(result);
+	})
+}
